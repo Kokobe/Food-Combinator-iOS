dd9c8baf03e6fd530317e4d5c99d14a55d6f345a
Jehan Kobe Chang <jehanc@uci.edu>
2021-03-19 11:12:38 -0700

    Initial Commit

---
+++ Food Combinator/Food_CombinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_CombinatorApp.swift
+//  Food Combinator
+//
+//  Created by Kobe Chang on 3/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Food_CombinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
