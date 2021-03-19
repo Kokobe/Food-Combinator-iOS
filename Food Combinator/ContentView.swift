55071e117f98db2b8eea9305c63790ac45fa0d9d
Jehan Kobe Chang <jehanc@uci.edu>
2021-03-19 12:08:24 -0700

    added button

--- Food Combinator/ContentView.swift
+++ Food Combinator/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Bro this is a food app")
+                .font(.headline)
+                .foregroundColor(Color.green)
+                .padding()
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Button")/*@END_MENU_TOKEN@*/
+            }
+        }
     }
 }
 
